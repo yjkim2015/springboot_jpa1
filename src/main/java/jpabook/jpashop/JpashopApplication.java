70ae12090159865c18c60b9f6f3f6e694216a416
kimyoonje <forteas2003@hanmail.net>
2022-03-19 17:26:00 +0900

    [김윤제] 간단한 주문 조회 V1

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,10 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.hibernate.Hibernate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -10,4 +13,17 @@ public class JpashopApplication {
         SpringApplication.run(JpashopApplication.class, args);
     }
 
+
+    /*
+       이거 쓰는거 좋은거아님
+       이런게 있다는것만 알려주는것..
+
+       DTO로 변환해서 반환하는것이 더 좋은 방법
+    */
+  /*  @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        ///hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
+    }*/
 }
